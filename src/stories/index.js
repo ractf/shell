1961a1ae8b893781d141d8225b484efed4e4d70b
Nick <nicholasgregory@live.co.uk>
2019-09-01 19:57:28 +0100

    Storybook Added & Components imported.

---
+++ src/stories/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import { Welcome } from '@storybook/react/demo';
+import Button from '../components/Button';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import HubButton from '../components/HubButton';
+import Input from '../components/Input';
+import Misc from '../components/Misc';
+import TabbedView from '../components/TabbedView';
+import Table from '../components/Table';
+import ToggleButton from '../components/ToggleButton';
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+storiesOf('Button', module)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}>
+      <span role="img" aria-label="so cool">
+        😀 😎 👍 💯
+      </span>
+    </Button>
+  ));
