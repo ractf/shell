959699fff93b9a3280430aec8416e7b5d887e236
Bottersnike <bottersnike237@gmail.com>
2019-09-01 23:56:04 +0100

    Cleanup storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,31 @@
-import { configure } from '@storybook/react';
+import React from 'react';
+
+import { addDecorator, configure } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
+
+import { GlobalStyle } from "../src/containers/controllers/App";
+import MockRoute from "./MockRoute";
+
+import styled from 'styled-components';
+const Centre = styled.div`
+    display: flex;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+    justify-content: center;
+
+    &>div {
+        flex-grow: 1;
+    }
+`;
+
+addDecorator(withKnobs);
+addDecorator(s => <><GlobalStyle /><MockRoute><Centre><div>{s()}</div></Centre></MockRoute></>);
+
+const req = require.context('../src/components', true, /\.stories\.js$/);
 
 function loadStories() {
-  require('../src/stories');
+    req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
