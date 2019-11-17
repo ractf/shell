c56dcd0be9fa53e159f6ba9f16e6d66012998f65
Bottersnike <bottersnike237@gmail.com>
2019-11-17 22:32:04 +0000

    Speed up CI

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const render = () => {
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
 
 render();
 
