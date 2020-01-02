2f68d7fffff8274f3205742cb2102eade28c0744
Bottersnike <bottersnike237@gmail.com>
2020-01-02 18:21:00 +0000

    Add modal close button for mobile

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -18,10 +18,14 @@ const Modal = ({ onHide, children, centre, small }) => {
     };
 
     return <div className={"modalWrap"}>
+        <style>{"@media (max-width: 930px) {body{overflow: hidden; height: 100vh;}}"}</style>
         <div className={"darken"} onMouseDown={(e => e.target.click())} onClick={doHide(onHide)} />
-        <div className={"modalBoxWrap"}><div className={"modalBox" + (centre ? " centre" : "") + (small ? " small" : "")}>
-            {children}
-        </div></div>
+        <div className={"modalBoxWrap"}>
+            <div className={"modalX"} onClick={doHide(onHide)}>&times;</div>
+            <div className={"modalBox" + (centre ? " centre" : "") + (small ? " small" : "")}>
+                {children}
+            </div>
+        </div>
     </div>;
 }
 
