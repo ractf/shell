4bbee55e45afe9c017b1f1d502dc9038f91da291
Bottersnike <bottersnike237@gmail.com>
2020-04-16 15:48:11 +0100

    Add marker to maps

--- src/plugins/standardChallenge/components/ClickableMap.js
+++ src/plugins/standardChallenge/components/ClickableMap.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Marker from "pigeon-marker";
 import Map from "pigeon-maps";
 
 import "./ClickableMap.scss";
@@ -6,49 +7,50 @@ import { FlashText } from "../../../components/Misc";
 
 
 export default ({ challenge, children }) => {
-    const zoomInCloserMessage = "Zoom in closer to make a selection!";
-
     const minZoomLevel = challenge.challenge_metadata.minimum_zoom_level || 10;
-    const [highlight, setHighlight] = useState(zoomInCloserMessage);
-    const [selectedLocation, setSelectedLocation] = useState("");
     const [hasValidZoom, setHasValidZoom] = useState(false);
     const [selectedLongLat, setSelectedLongLat] = useState(null);
 
     const click = (e) => {
         if (hasValidZoom) {
             setSelectedLongLat(e.latLng);
-            setHighlight("Selected location: ");
-            setSelectedLocation(`${e.latLng[0]}, ${e.latLng[1]}`);
         } else {
-            setHighlight(zoomInCloserMessage);
+            setSelectedLongLat(null);
         }
     };
 
     const onMapMove = (e) => {
         if (e["zoom"] > minZoomLevel) {
-            if (!selectedLongLat) {
-                setHighlight("Click on the map to select a location");
-            };
             setHasValidZoom(true);
         } else {
-            if (!selectedLongLat) {
-                setHighlight(zoomInCloserMessage);
-                setSelectedLocation("");
-            };
             setHasValidZoom(false);
         }
     };
 
+    const round = (num) => {
+        return Math.round(num * 1000000) / 1000000;
+    }
+
     return <div className={"mapPartWrap"}>
         <FlashText>
             <div className={"selectedParent"}>
-                <div className="highlight">{highlight}</div>
-                <div className="selectedLocation">{selectedLocation}</div>
+                {selectedLongLat ? <>
+                    <div className="highlight">
+                        Selected location: {round(selectedLongLat[0])}, {round(selectedLongLat[1])}
+                    </div>
+                </> : hasValidZoom ? <>
+                    <div className="highlight">Click on the map to select a location</div>
+                </> : <>
+                    <div className="highlight">Zoom in closer to make a selection!</div>
+                </>}
+                
             </div>
         </FlashText>
         <div className={"mapWrap"}>
             <Map className={"clickableMapMap"} center={[45.04, -4.04]}
-                defaultZoom={4} onClick={click} onBoundsChanged={onMapMove} />
+                defaultZoom={4} onClick={click} onBoundsChanged={onMapMove}>
+                {selectedLongLat && <Marker paylod={1} anchor={selectedLongLat} />}
+            </Map>
         </div>
     </div>;
 };
