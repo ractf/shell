36a86b3dc41856be5c7c0753c29b0ab1dfc419d4
Bottersnike <Bottersnike@users.noreply.github.com>
2020-06-02 00:02:06 +0100

    Update setup.js

--- src/plugins/sentry/setup.js
+++ src/plugins/sentry/setup.js
@@ -6,7 +6,7 @@ import { registerPlugin } from "ractf";
 export default () => {
     let dsn = process.env.REACT_APP_SENTRY_DSN;
     if (dsn) {
-        Sentry.init({dsn: dsn});
+        Sentry.init({dsn: dsn, release: "{__COMMIT_HASH__}".substring(0, 12)});
 
         registerPlugin("errorHandler", "sentry", (error, errorInfo) => {
             Sentry.captureException(error, { extra: errorInfo });
