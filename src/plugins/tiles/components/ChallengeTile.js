e91b9381e25a197fd21809c66f4ee449bb141ece
Bottersnike <bottersnike237@gmail.com>
2020-09-05 02:32:12 +0100

    Add start of tiles plugin

---
+++ src/plugins/tiles/components/ChallengeTile.js
@@ -0,0 +1,69 @@
+// Copyright (C) 2020 Really Awesome Technology Ltd
+//
+// This file is part of RACTF.
+//
+// RACTF is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// RACTF is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with RACTF.  If not, see <https://www.gnu.org/licenses/>.
+
+import React, { useCallback } from "react";
+
+import { Markdown } from "@ractf/ui-kit";
+
+import style from "./ChallengeTile.module.scss";
+import { makeClass } from "@ractf/util";
+
+
+const DIFFICULTIES = [
+    // Points, Name, Colour
+    [50, "Trivial", "blue"],
+    [100, "Easy", "green"],
+    [200, "Medium", "orange"],
+    [500, "Had", "red"],
+    [1000, "Insane", "purple"],
+];
+
+
+const ChallengeTile = ({ setChallenge, challenge }) => {
+    let difficulty = 0;
+    for (let i = 0; i < DIFFICULTIES.length; i++) {
+        if (challenge.score >= DIFFICULTIES[i][0])
+            difficulty = i;
+    }
+    const onClick = useCallback(() => {
+        setChallenge(challenge);
+    }, [setChallenge, challenge]);
+
+    return <div className={makeClass(style.tile, challenge.solved && style.solved)} onClick={onClick}>
+        <div className={style.tileHead}>
+            <div className={style.headMain}>
+                <div className={style.title}>{challenge.name}</div>
+                <div className={makeClass(style.subtitle, style[DIFFICULTIES[difficulty][2]])}>
+                    <div className={style.dots}>
+                        {([...new Array(difficulty)]).map((i, n) => (
+                            <div className={style.dot} key={n} />
+                        ))}
+                    </div>
+                    <span>{DIFFICULTIES[difficulty][1]}</span>
+                </div>
+            </div>
+            <div>
+                <div className={style.points}>{challenge.score}</div>
+                <div className={style.pointsSub}>points</div>
+            </div>
+        </div>
+        <div className={style.tileBody}>
+            <Markdown source={challenge.description} />
+        </div>
+    </div>;
+};
+export default React.memo(ChallengeTile);
---
+++ src/plugins/tiles/components/TilesChallenges.js
@@ -0,0 +1,130 @@
+// Copyright (C) 2020 Really Awesome Technology Ltd
+//
+// This file is part of RACTF.
+//
+// RACTF is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// RACTF is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with RACTF.  If not, see <https://www.gnu.org/licenses/>.
+
+import React, { useEffect, useState } from "react";
+
+import { Column, Row, Input, FormGroup, Button, Modal } from "@ractf/ui-kit";
+
+import ChallengePage from "pages/ChallengePage.js";
+import ChallengeTile from "./ChallengeTile.js";
+
+import style from "./ChallengeTile.module.scss";
+
+
+export const TilesChallenges = ({ challenges: category, showEditor, isEdit, showLocked }) => {
+    const [challenge, setChallenge] = useState(null);
+    const [filter, setFilter] = useState({});
+    const [search, setSearch] = useState("");
+
+    const tags = {};
+
+    const shouldShow = (challenge, options = {}) => {
+
+        if (!options.ignoreTags && Object.keys(filter).length !== 0) {
+            let hasATag = false;
+            for (const i of challenge.tags) {
+                if (filter[i]) {
+                    hasATag = true;
+                    break;
+                }
+            }
+            if (!hasATag)
+                return false;
+        }
+
+        if (search === null || search.length === 0)
+            return true;
+        if (challenge.name.toLowerCase().indexOf(search.toLowerCase()) !== -1)
+            return true;
+        if (challenge.description.toLowerCase().indexOf(search.toLowerCase()) !== -1)
+            return true;
+
+        return false;
+    };
+    for (const challenge of category.challenges) {
+        for (const tag of challenge.tags) {
+            if (!tags[tag]) tags[tag] = 0;
+            if (shouldShow(challenge, { ignoreTags: true })) {
+                tags[tag]++;
+            }
+        }
+    }
+
+    const toggleFilter = (i) => {
+        return () => {
+            const newFilter = { ...filter };
+            if (filter[i]) delete newFilter[i];
+            else newFilter[i] = true;
+
+            setFilter(newFilter);
+        };
+    };
+    // Refresh filter
+    useEffect(() => {
+        const newFilter = { ...filter };
+        let changed = false;
+        for (const i in filter) {
+            if ((typeof tags[i]) === "undefined") {
+                if (filter[i]) {
+                    delete newFilter[i];
+                    changed = true;
+                }
+            }
+        }
+        if (changed)
+            setFilter(newFilter);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [tags]);
+
+    const sortedTags = Object.keys(tags).sort((a, b) => a.localeCompare(b)).map(i => [i, tags[i]]);
+
+    return <Column>
+        {challenge && (
+            <Modal transparent onClose={() => setChallenge(null)} noCancel header={challenge.title}
+                okayLabel={"Back"} onConfirm={() => setChallenge(null)}
+            >
+                <ChallengePage tabId={category.id} chalId={challenge.id} />
+            </Modal>
+        )}
+        <FormGroup>
+            <Input onChange={setSearch} value={search}
+                name={"search"} placeholder={"Search challenges"} val={search} managed />
+        </FormGroup>
+        <FormGroup>
+            {sortedTags.length !== 0 && (
+                <Row>
+                    {sortedTags.map(([tag, tagCount]) => (
+                        <Button key={tag}
+                            Icon={props => <span {...props}>{tagCount}</span>}
+                            onClick={toggleFilter(tag)} success={filter[tag]}
+                        >{tag}</Button>
+                    ))}
+                </Row>
+            )}
+        </FormGroup>
+        <FormGroup>
+            <div className={style.tiles}>
+                {category.challenges.sort((x, y) => x.score - y.score).map(
+                    i => (shouldShow(i) ? <ChallengeTile
+                        category={category} key={i.id} challenge={i}
+                        setChallenge={setChallenge}
+                    /> : null)
+                )}
+            </div>
+        </FormGroup>
+    </Column>;
+};
