f0c41ce8f445db6710f9ad5d79b9834555adf60a
Nick <33908215+NicholasG04@users.noreply.github.com>
2022-02-28 10:00:54 +0000

    chore: add points_type

--- src/plugins/standardChallenge/components/Editor.js
+++ src/plugins/standardChallenge/components/Editor.js
@@ -462,6 +462,10 @@ const Editor = ({ challenge, category, isCreator, saveEdit, removeChallenge, emb
                             <Input rows={3} val={challenge.post_score_explanation} name={"post_score_explanation"}
                                 placeholder={t("editor.post_score_explanation")} />
                         </Form.Group>
+                        <Form.Group htmlFor={"points_type"} label={t("editor.points_type")}>
+                            <Select initial={challenge.points_type ?? 'basic'}
+                                name={"points_type"} options={['basic', 'decay']} />
+                        </Form.Group>
                         <Checkbox val={isCreator || (!!challenge.tiebreaker)} name={"tiebreaker"}>
                             {t("editor.tiebreaker")}
                         </Checkbox>
