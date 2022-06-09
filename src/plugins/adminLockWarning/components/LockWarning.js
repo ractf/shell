f5feb92847754ee2b4e5fb18055c399368246377
0x4da <me@ada.rip>
2022-06-09 22:54:21 +0100

    Add admin config warnings (#75)
    * Add admin config warning
    
    * Use i18n and add more warnings

--- src/plugins/adminLockWarning/components/LockWarning.js
+++ src/plugins/adminLockWarning/components/LockWarning.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { MdWarning } from "react-icons/md";
+import { useTranslation } from "react-i18next";
 
 import { ToggleTab } from "@ractf/ui-kit";
 
@@ -8,16 +9,17 @@ import { ToggleTab } from "@ractf/ui-kit";
 const LockWarning = () => {
     const user = useSelector(state => state.user);
     const countdown_passed = useSelector(state => state.countdowns?.passed) || {};
+    const { t } = useTranslation();
     
     if (!user || !user.is_staff) return null;
 
     if (!countdown_passed.registration_open)
         return <ToggleTab Icon={MdWarning} danger>
-            Registration locked!
+            { t("admin.warnings.registration_locked") }
         </ToggleTab>;
     if (!countdown_passed.countdown_timestamp)
         return <ToggleTab Icon={MdWarning} warning>
-            Challenges locked!
+            { t("admin.warnings.challenges_locked") }
         </ToggleTab>;
     return null;
 };
